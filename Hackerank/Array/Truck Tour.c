cdc1448330697944fabdb87c70e7a9bf13acd6e1
an-overthinker <raiadityavardhan@gmail.com>
2023-08-08 21:01:03 +0530

    daily

---
+++ Hackerank/Array/Truck Tour.c
@@ -0,0 +1,193 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+char** split_string(char*);
+
+int parse_int(char*);
+
+/*
+ * Complete the 'truckTour' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY petrolpumps as parameter.
+ */
+
+int truckTour(int petrolpumps_rows, int petrolpumps_columns, int** petrolpumps)
+{
+    int i,temp,fuel=0;
+    for(i=0;i<petrolpumps_rows;i++)
+    {
+        fuel+=petrolpumps[i][0];
+        if(fuel<petrolpumps[i][1])
+        {
+            temp=i+1;
+            fuel=0;
+        }
+        else
+            fuel-=petrolpumps[i][1];
+    }
+    return temp;
+}
+
+int main()
+{
+	int i,j;
+	
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    int n = parse_int(ltrim(rtrim(readline())));
+
+    int** petrolpumps = malloc(n * sizeof(int*));
+
+    for (i = 0; i < n; i++) {
+        *(petrolpumps + i) = malloc(2 * (sizeof(int)));
+
+        char** petrolpumps_item_temp = split_string(rtrim(readline()));
+
+        for (j = 0; j < 2; j++) {
+            int petrolpumps_item = parse_int(*(petrolpumps_item_temp + j));
+
+            *(*(petrolpumps + i) + j) = petrolpumps_item;
+        }
+    }
+
+    int result = truckTour(n, 2, petrolpumps);
+
+    fprintf(fptr, "%d\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
+
