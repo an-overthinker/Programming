424725576042c08239f0ed1622fd1b9c76fee1b8
an-overthinker <raiadityavardhan@gmail.com>
2023-09-20 21:41:38 +0530

    daily

---
+++ Hackerank/Problem Solving/Algorithms/Implementation/Sherlock and Squares.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'squares' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER a
+#  2. INTEGER b
+#
+def squares(a, b):
+    count_squares = 0
+    for i in range(int(math.sqrt(a)),int(math.sqrt(b))+1):
+        if i*i in range(a,b+1):
+            count_squares = count_squares + 1
+    return count_squares
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        first_multiple_input = input().rstrip().split()
+
+        a = int(first_multiple_input[0])
+
+        b = int(first_multiple_input[1])
+
+        result = squares(a, b)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
