8eb31293e311c709cfb41f4baf4d59f4ebaf42cd
an-overthinker <raiadityavardhan@gmail.com>
2023-09-22 23:03:12 +0530

    daily

---
+++ Hackerank/Python/Strings/Merge the Tools!.py
@@ -0,0 +1,12 @@
+def merge_the_tools(string, k):
+    while string:
+        temp = ''
+        chunk, string = string[:k], string[k:]
+        for i in chunk:
+            if i not in temp:
+                temp += i
+        print(temp)
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
